6bb75008de5a392774bf71f018dfa3d17fd7924b
Dr-mkelvo <kelvincent002@gmail.com>
2023-09-07 22:51:30 +0300

    backend setup auth page -Login and signup

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,28 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const dotenv  = require("dotenv").config()
-const cors = require(cors)
+const dotenv = require('dotenv').config()
+const cors = require("cors") // helps the server to communicate with the client
+const authController = require("./controllers/authController")
+
+ const app = express()
+
+ //mongodb connect
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URL)
+
+
+//routes/ middlewares
+app.use(cors())
+app.use(express.json)
+app.use(express.urlencoded({extended: true}))
+app.use("auth",authController)
+
+
+ //starting server
+app.listen(process.env.PORT, () => console.log("Server is up"))
+
+// {"username": "username1",
+// "email": "username1@gmail.com",
+// "password": "password4username1"
+// }
+// http://localhost:5000/auth/register
